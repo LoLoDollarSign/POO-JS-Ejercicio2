31f09a3074674315e2ce1e547ba795cf06a207d3
LoLoDollarSign <sergey.mamani@gmail.com>
2026-04-22 08:24:17 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
-// Definición de la clase (Molde) [1]
+
 class Mascota {
     constructor(nombre, tipo) {
         this.nombre = nombre;
         this.tipo = tipo;
-        this.energia = 100; // Estado inicial [5]
+        this.energia = 100; 
     }
 
-    // Métodos: Acciones que modifican las propiedades [6]
+ 
     jugar() {
         this.energia -= 20;
         if (this.energia < 0) this.energia = 0; // Validación de rango
@@ -26,20 +26,20 @@ class Mascota {
     }
 }
 
-// Crear la instancia única [7]
+
 const miMascota = new Mascota("Fido", "Perro");
 
-// Función para conectar el objeto con la página web (DOM) [2]
+
 function actualizarPantalla() {
     document.getElementById("info").innerText = miMascota.estado();
     const barra = document.getElementById("barra");
     barra.style.width = miMascota.energia + "%";
 }
 
-// Funciones que llaman a los métodos del objeto
+
 function hacerJugar() { miMascota.jugar(); actualizarPantalla(); }
 function hacerComer() { miMascota.comer(); actualizarPantalla(); }
 function hacerDormir() { miMascota.dormir(); actualizarPantalla(); }
 
-// Mostrar estado inicial
+
 actualizarPantalla();
